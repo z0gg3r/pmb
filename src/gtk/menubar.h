9f808cc9e952651fdb3cf11cdf7324d540ef3faa
zocker <zocker@10zen.eu>
2022-10-22 17:41:37 +0200

    Remove gpmb
    Away with it, I say, Away!

--- src/gtk/menubar.h
+++
@@ -1,9 +0,0 @@
-#ifndef MENUBAR_H
-#define MENUBAR_H
-
-#include "interface.h"
-
-GtkWidget*
-menu_bar_new(GtkWidget*);
-
-#endif
--- src/gtk/toolbox.h
+++
@@ -1,9 +0,0 @@
-#ifndef TOOLBOX_H
-#define TOOLBOX_H
-
-#include "interface.h"
-
-GtkWidget*
-tool_box_new(GtkWidget*); 
-
-#endif
